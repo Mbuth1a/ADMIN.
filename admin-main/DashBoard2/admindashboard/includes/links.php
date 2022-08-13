dad8125e1fe98774276784b86aebbf7ef05b65aa
mbuthia <petermbuthia112@gmail.com>
2022-08-13 12:01:50 +0300

    done

--- admin-main/DashBoard2/admindashboard/includes/links.php
+++ admin-main/DashBoard2/admindashboard/includes/links.php
@@ -3,7 +3,7 @@
 	<title>Bootstrap Admin Template</title>
 	<meta charset="UTF-8">
 	<meta name="description" content="Creating admin dashboard">
-	<meta name="keywords" content="HTML,CSS,Zalego,Technology,Zalego institute,JavaScript">
+	<meta name="keywords" content="HTML,CSS,Zalego,Fintech, Technology,Zalego institute,JavaScript">
 	<meta name="author" content="Peter Mbuthia">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
